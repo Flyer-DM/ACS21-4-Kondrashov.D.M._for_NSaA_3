1d7166148292996990d5368146b0dd5f6ff1a8cf
Dmitry <lexa.gorky@yandex.ru>
2022-10-26 20:00:53 +0300

    made multiplying func, list reshape func, multiprocessing connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-#import multiprocessing
-from typing import List, Union
+import multiprocessing
+from typing import List
+from numpy import array, reshape
 
 
 def file_opener(func):
@@ -12,16 +13,16 @@ def file_opener(func):
     return wrapper
 
 
-MATRIX = List[List[Union[int, float]]]
+MATRIX = List[List[int]]
 
 
-def calc_elem(i_a: int, i_b: int, m_a: MATRIX, m_b: MATRIX) -> Union[int, float]:
+def calc_elem(i_a: int, i_b: int, m_a: MATRIX, m_b: MATRIX, queue: multiprocessing.Queue):
     """Функция вычисление элемента матрицы при перемножении матриц"""
-    n: int = len(m_a[0])
-    result: Union[int, float] = 0
+    n = len(m_a[0])
+    result = 0
     for i in range(n):
         result += m_a[i_a][i] * m_b[i][i_b]
-    return result
+    queue.put(result)
 
 
 @file_opener
@@ -34,7 +35,7 @@ def read_matrix(path: str, sep=';') -> MATRIX:
         for string in str_matrix:
             gap_matrix = []
             for number in string.split(', '):
-                gap_matrix.append(float(number) if '.' in number else int(number))
+                gap_matrix.append(int(number))
             int_matrix.append(gap_matrix)
         return int_matrix
 
@@ -53,12 +54,30 @@ def write_elem(path: str, i: int, j: int, sep=';') -> None:
     pass
 
 
+def list_reshaper(matrix_list: list, i: int, j: int) -> MATRIX:
+    """Функция преобразования списка чисел в матрицу"""
+    matrix_list = reshape(array(matrix_list), (i, j))
+    result: MATRIX = []
+    for elem in matrix_list:
+        result.append(list(elem))
+    return result
+
+
 def multiply(m_a: MATRIX, m_b: MATRIX) -> MATRIX:
     """Функция переумножающая матрицы"""
-    pass
+    queue = multiprocessing.Queue()
+    result = []
+    lines, columns = len(m_a), len(m_b[0])
+    for i in range(len(m_a)):
+        for j in range(len(m_a[0])):
+            process_elem = multiprocessing.Process(target=calc_elem, args=(i, j, m_a, m_b, queue))
+            process_elem.start()
+            result.append(queue.get())
+            process_elem.join()
+    return list_reshaper(result, lines, columns)
 
 
-
-# load_matrix([[1, 2.6, 7], [3.3, 4, 8], [5, 6, 10.5]], "D:\ССиС и ПпП\pythonProject_multiprocessing\\test.txt")
-# a = read_matrix("D:\ССиС и ПпП\pythonProject_multiprocessing\\test.txt")
-# print(a)
+if __name__ == '__main__':
+    a = [[1, 2], [3, 4]]
+    b = [[4, 5], [6, 7]]
+    print(multiply(a, b))
